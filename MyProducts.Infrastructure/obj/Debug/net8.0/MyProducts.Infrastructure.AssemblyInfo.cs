5a8fa4a87548ab7e839ff723e541b8fb8838cebf
EduLp99 <edulp99@gmail.com>
2025-03-31 04:46:33 -0300

    initial commit

--- MyProducts.Infrastructure/obj/Debug/net8.0/MyProducts.Infrastructure.AssemblyInfo.cs
+++ MyProducts.Infrastructure/obj/Debug/net8.0/MyProducts.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProducts.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33e94fc5b6d966e5510e9ef5eb93b531550033a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProducts.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProducts.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
