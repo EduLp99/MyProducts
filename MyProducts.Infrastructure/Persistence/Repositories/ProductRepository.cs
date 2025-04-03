4a3fb5af13fbb3050f773ddc827bc6a2bba76706
Eduardo Felipe <69924391+EduLp99@users.noreply.github.com>
2025-04-03 13:00:42 -0300

    Update ProductRepository.cs

--- MyProducts.Infrastructure/Persistence/Repositories/ProductRepository.cs
+++ MyProducts.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -4,15 +4,26 @@ using MyProducts.Core.Repositories;
 
 namespace MyProducts.Infrastructure.Persistence.Repositories;
 
+/// <summary>
+/// Repositório responsável pelo acesso e manipulação dos dados da entidade Product.
+/// </summary>
 public class ProductRepository : IProductRepository
 {
     private readonly MyProductsDbContext _dbContext;
 
+    /// <summary>
+    /// Construtor da classe ProductRepository.
+    /// </summary>
+    /// <param name="dbContext">Contexto do banco de dados utilizado para acessar os produtos.</param>
     public ProductRepository(MyProductsDbContext dbContext)
     {
         _dbContext = dbContext;
     }
 
+    /// <summary>
+    /// Obtém todos os produtos cadastrados no banco de dados.
+    /// </summary>
+    /// <returns>Uma coleção de produtos.</returns>
     public async Task<IEnumerable<Product>> GetAll()
     {
         var products = await _dbContext.Products.ToListAsync();
